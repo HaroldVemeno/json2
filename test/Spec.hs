a9420202191861e2f6866d0264dd938d320a65bd
Miky <haroldvemeno@email.cz>
2021-04-07 18:49:27 +0200

    an enviromental variable for changing the test file

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,10 @@
 module Main where
 
 import CLI ( timeAll )
+import System.Environment
+import Data.Maybe (fromMaybe)
 
 main :: IO ()
-main = timeAll ["test/test.json"]
+main = do
+  file <- fromMaybe "test/test.json" <$> lookupEnv "TEST_FILE" 
+  timeAll [file]
